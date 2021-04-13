81da756f4bd0977be093a79b75ae135b5768a61d
Darius Seroka <dariusjs@gmail.com>
2021-04-13 22:20:09 +0200

    some regex stuff

--- src/index5.ts
+++ src/index5.ts
@@ -1,7 +1,46 @@
-export const xx = {};
+import { pipe } from 'fp-ts/lib/pipeable';
+import { either } from 'fp-ts';
+import { chain, Either } from 'fp-ts/lib/Either';
+import { map } from 'fp-ts/lib/ReadonlyRecord';
 
-if (JSON.stringify(xx) === JSON.stringify({})) {
-  console.log(JSON.stringify(xx) === JSON.stringify({}) ? 0 : xx);
-} else {
-  console.log('not');
+const houseSample1 = '100A';
+const houseSample2 = '10-A';
+const houseSample3 = '10 A';
+const houseSample4 = '10/A';
+const houseSample5 = '10-2';
+
+function parseHouseNumber(input: any) {
+  const houseNumber = input.match(/^\d+/g);
+  const houseNumberExtra = input.match(/[a-zA-Z]/g);
+
+  return {
+    houseNumber: houseNumber ? parseFloat(houseNumber[0]) : 0,
+    houseNumberAddition: houseNumberExtra ? houseNumberExtra[0] : ''
+  };
 }
+
+console.log(parseHouseNumber(houseSample1));
+console.log(parseHouseNumber(houseSample2));
+console.log(parseHouseNumber(houseSample3));
+console.log(parseHouseNumber(houseSample4));
+console.log(parseHouseNumber(houseSample5));
+
+const houseNumber = (input: string) => {
+  const houseNumber = input.match(/^\d+/g);
+  return houseNumber ? parseInt(houseNumber[0]) : 0;
+};
+
+const houseNumberPostfix = (input: string) => {
+  const houseNumberExtra = input.match(/[a-zA-Z]|(\b \d+)|(\b-\d+)|(\b\.\d+)/g);
+  return houseNumberExtra ? houseNumberExtra[0].replace(/(\W)/g, '') : null;
+};
+
+console.log(houseNumber('10A'));
+
+console.log(houseNumberPostfix('10A'));
+console.log(houseNumberPostfix('10-A'));
+console.log(houseNumberPostfix('10\\A'));
+console.log(houseNumberPostfix('10-30'));
+console.log(houseNumberPostfix('10.300'));
+console.log(houseNumberPostfix('10 300'));
+console.log(houseNumberPostfix('10'));
